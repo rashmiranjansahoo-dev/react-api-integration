1e71692c6d23f1ce6099afb0dd5594ce664a164e
Rashmiranjan Sahoo <rasmisahooraja@gmail.com>
2026-02-28 17:06:06 +0530

    update users API implementation

---
+++ src/api/usersApi.js
@@ -0,0 +1,19 @@
+const API_URL = "https://jsonplaceholder.typicode.com/users";
+
+export async function fetchUsers() {
+  const response = await fetch(API_URL);
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch users");
+  }
+
+  const data = await response.json();
+
+  return data.map(user => ({
+    id: user.id,
+    name: user.name,
+    email: user.email,
+    company: user.company.name,
+    city: user.address.city,
+  }));
+}
